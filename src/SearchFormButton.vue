2066c3bc4bcae6e7a44ca4f97be5432b2a9f493f
andrunix <andrunix@gmail.com>
2018-06-28 15:33:41 -0400

    Added reference to github repo

---
+++ src/SearchFormButton.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="search-form-button" @click="$emit('click')">
+  <i class="fa fa-search" aria-hidden="true"></i>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'search-form-button'
+};
+</script>
+
+<style>
+.search-form-button {
+  background-color: #ff9800;
+  color: #fff;
+  min-height: 40px;
+  min-width: 40px;
+  max-width: 40px;
+  max-height: 40px;
+  border-radius: 25px;
+  text-align: center;
+  padding-top: 9px;
+  box-shadow: 1px 2px 4px 0 rgba(0, 0, 0, 0.4);
+  position: absolute;
+  right: 7px;
+  bottom: -10px;
+}
+.search-form-button:hover {
+  box-shadow: none;
+  cursor: pointer;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'src'),
+      'node_modules'
+    ],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['.js', '.json', '.vue']
+  },
+  entry: './src/SearchFormButton.vue',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'search-form-button.min.js',
+    library: 'SearchFormButton',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: path.resolve(__dirname, 'node_modules')
+      }
+    ]
+  },
+  devtool: false,
+  performance: {
+    hints: false
+  }
+};
